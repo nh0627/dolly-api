f2147fc98aef1ca469db8c0f6fda70e64a067837
Nahyeon Lee <naboee0627@gmail.com>
2020-02-21 21:10:19 +0900

    Handel 404 error

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+// import libraries
 import express from 'express'
+import createError from 'http-errors'
+
 const app = express()
 const port = process.env.PORT || 3000
-const ROUTES_PATH = './routes'
 
-app.use('/', require(`${ROUTES_PATH}`))
-app.use('/item', require(`${ROUTES_PATH}/item`))
+// define router folders
+const itemRouter = require('./routes/item')
 
+// set routes
+app.use(['/item', '/api/item'], itemRouter)
+// add router here ! ! ! ★
+
+// set a server listener
 app.listen(port, () => {
   console.log('Dolly server is up on', port)
 })
 
-module.exports = {
-  path: '/api',
-  handler: app
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err.status, message: res.locals.message });
+});
+
+module.exports = app
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.get('/', (req, res, next) => {
-    res.send('작동 여부 확인' + Math.random())
-})
-
-module.exports = router
